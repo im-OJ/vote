e0e85aedbf44096fd79d2685488b4522d67cbba9
im-OJ <44771710+im-OJ@users.noreply.github.com>
2019-04-02 14:38:22 +0100

    aa

--- back/config.php
+++ back/config.php
@@ -3,5 +3,4 @@ $SQL_SERVER = "127.0.0.1";
 $SQL_USER = "root";
 $SQL_PASS = "speaker";
 $SQL_DB = 'vote';
-echo "test";
  ?>
--- back/getImages.php
+++ back/getImages.php
@@ -5,7 +5,7 @@ include('DB_Connect.php');
 $MINIMUM_SHARE_SCORE = 900;
 
 echo "hello";
-echo var_dump(query_DB("SELECT * FROM posts"));
+echo query_DB("SELECT * FROM posts");
 
 
 echo "hello";
